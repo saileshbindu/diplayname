37b61a817d230aacf3f5e0557c6d6fe718585697
saileshbindu <sailesh.bindu189@gmail.com>
2024-05-08 17:54:05 +0530

    displayName first push

---
+++ src/DisplayName.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const DisplayFullName = () =>{
+const [firstName, setFirstName] = useState('')
+const [lastName, setLastName] = useState('')
+const [fullNamedisplay, setFullNameDisplay] = useState('')
+
+const handleFirstName = (event) =>{
+    setFirstName(event.target.value)
+}
+const handleLastName = (event) =>{
+    setLastName(event.target.value)
+}
+
+const handleFullname = (event) =>{
+    event.preventDefault();
+    setFullNameDisplay(`${firstName} ${lastName}`)
+    setFirstName('')
+    setLastName('')
+}
+
+return(<div>
+    <h1>Full Name Display</h1>
+    <form onSubmit={handleFullname}>
+        <div style={{marginBottom:"10px"}}>
+            <label>Full Name: </label>
+            <input type="name" value={firstName} onChange={handleFirstName} required />
+        </div>
+        <div style={{marginBottom:"10px"}}> 
+            <label>Last Name: </label>
+            <input type="name" value={lastName} onChange={handleLastName} required />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+
+    {fullNamedisplay  && <p>Full Name : {fullNamedisplay}</p>}
+</div>)
+}
+
+export default DisplayFullName;
